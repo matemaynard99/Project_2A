7ff217273f497783bdb72dcc1f6a78e6d357593e
matemaynard99 <78230596+matemaynard99@users.noreply.github.com>
2021-02-04 21:16:11 -0600

    Update Project_2A.java

--- Project_2A.java
+++ Project_2A.java
@@ -104,11 +104,18 @@ public class Project_2A {
 		}
 	}//end evaluate method
 	
-	public int precedence(String s) {
-		/**
-		 * FIX ME
-		 */
-	}
+	public class Precedence {
+		public static int precedence(String operator) {
+			if (operator.equals("^"))                           { return 7; }
+			if (operator.equals("*") || operator.equals("/"))   { return 6; }
+			if (operator.equals("+") || operator.equals("-"))   { return 5; }
+			if (operator.equals(">") || operator.equals(">="))  { return 4; }
+			if (operator.equals("<") || operator.equals("<="))  { return 4; }
+			if (operator.equals("==") || operator.equals("!=")) { return 3; }
+			if (operator.equals("&&"))                          { return 2; }
+			if (operator.equals("||"))                          { return 1; }		
+			throw new IllegalArgumentException(String.format("Operator %s is not supported.", operator)) }  // Time complexity: O(n)
+        }
 	
 	public int equation(Integer left, Integer right, String s) {
 		/**
