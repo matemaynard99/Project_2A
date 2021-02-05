f37189b4a1205fbb8da5676af4b406ffcd4b9073
matemaynard99 <78230596+matemaynard99@users.noreply.github.com>
2021-02-04 21:24:10 -0600

    Update Project_2A.java

--- Project_2A.java
+++ Project_2A.java
@@ -104,18 +104,23 @@ public class Project_2A {
 		}
 	}//end evaluate method
 	
-	public class Precedence {
-		public static int precedence(String operator) {
-			if (operator.equals("^"))                           { return 7; }
-			if (operator.equals("*") || operator.equals("/"))   { return 6; }
-			if (operator.equals("+") || operator.equals("-"))   { return 5; }
-			if (operator.equals(">") || operator.equals(">="))  { return 4; }
-			if (operator.equals("<") || operator.equals("<="))  { return 4; }
-			if (operator.equals("==") || operator.equals("!=")) { return 3; }
-			if (operator.equals("&&"))                          { return 2; }
-			if (operator.equals("||"))                          { return 1; }		
-			throw new IllegalArgumentException(String.format("Operator %s is not supported.", operator)) }  // Time complexity: O(n)
-        }
+		/*
+		*Precedence method. This returns a value based on the operator 
+		*This value represents the order of operations on the expression so the proper result can be calculated
+		*Takes in the string operater and compares it using operator.equals to find the proper int value of precedence
+		*@Returns: Int representing precendence 
+		*/
+	public int precedence(String operator) {
+		if (operator.equals("^"))                           { return 7; }
+		if (operator.equals("*") || operator.equals("/"))   { return 6; }
+		if (operator.equals("+") || operator.equals("-"))   { return 5; }
+		if (operator.equals(">") || operator.equals(">="))  { return 4; }
+		if (operator.equals("<") || operator.equals("<="))  { return 4; }
+		if (operator.equals("==") || operator.equals("!=")) { return 3; }
+		if (operator.equals("&&"))                          { return 2; }
+		if (operator.equals("||"))                          { return 1; }		
+		throw new IllegalArgumentException(String.format("Operator %s is not supported.", operator)) }  // Time complexity: O(n)
+        }//End Precedence method
 	
 	public int equation(Integer left, Integer right, String s) {
 		/**
