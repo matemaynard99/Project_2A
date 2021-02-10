42ae11c4de59f484ee4092f9f5b77916dd9e6e3a
matemaynard99 <78230596+matemaynard99@users.noreply.github.com>
2021-02-10 09:05:01 -0600

    Update Project_2A.java

--- Project_2A.java
+++ Project_2A.java
@@ -6,8 +6,8 @@
 package Project_2A;
 import java.util.*;
 public class Project_2A {
-	
-	public int evaluatEquation(String s) {
+	private static int evaluateEquation(String s) {
+		s = s + " ";
 		Stack<Integer> nums = new Stack<Integer>();
 		Stack<String> operators = new Stack<String>();
 		char[] tokenArray = s.toCharArray();
@@ -19,7 +19,7 @@ public class Project_2A {
 		 * seperately.
 		 */
 		//use for loop to iterate through whole equation
-		for (int i = 0; i<tokenArray.length; i++) {
+		for (int i = 0; i<tokenArray.length-1; i++) {
 			//simple line to ignore spaces
 			if (tokenArray[i] == ' ') {
 				continue;
@@ -27,16 +27,18 @@ public class Project_2A {
 			
 			//check if current character is a number
 			if (isNumber(tokenArray[i])) {
+				System.out.println(tokenArray[i]);
 				/**converted char to string so it can become integer
 				 * unsure how to change char to int directly
 				 */
 				//added while loop to catch multidigit numbers
+				String newNum = "";
 				while(isNumber(tokenArray[i])){
-					String newNum = "";
 					newNum = newNum + tokenArray[i];
 					i++;
 					//add to stack of numbers as an integer
 					nums.push(Integer.parseInt(newNum));
+					System.out.println(nums);
 				}//end while
 				/** i has to be decrimented, since it is incremented in the while loop
 				 * that way the main for loop doesnt skip any tokens in the equation
@@ -52,22 +54,21 @@ public class Project_2A {
 			//check for closing parenthesis
 			else if(tokenArray[i] == ')') {
 				
-				while(operators.peek() != ")") {
-					/**Performsoperation in the parenthesis in order
+				while(operators.peek() != "(") {
+					/**Performs operation in the parenthesis in order
 					 * until it reaches the open parenthesis
 					 * pushes the outcome into the numbers stack after each operation
 					 */
 					Integer right = nums.pop();
+					//System.out.println(nums);
 					Integer left = nums.pop();
 					nums.push(equation(left, right, operators.pop()));
 				}//end while
 			}//end branch for closing parenthesis
-			else if(isOperator(tokenArray[i])) {
-				/**while loop to find multipart operators
-				 * and convert them to a string
-				 */
-				string newOp = "";
-				while(isOperator(tokenArray[i])) {
+			else{
+				
+				String newOp = "";
+				while(!isNumber(tokenArray[i]) && tokenArray[i]!= ' ') {
 					newOp = newOp + tokenArray[i];
 					i++;
 					
@@ -76,7 +77,22 @@ public class Project_2A {
 				 * that way the main for loop doesnt skip any tokens in the equation
 				 */
 				i--;
-				
+				if(isOperator(newOp)) {
+					if(operators.isEmpty()) {
+						operators.push(newOp);
+						System.out.println(operators);
+					}
+					else {
+						while(!operators.isEmpty() && (precedence(operators.peek()) > precedence(newOp))) {
+							Integer right = nums.pop();
+							Integer left = nums.pop();
+							nums.push(equation(left, right, operators.pop()));
+						}//end while loop
+						operators.push(newOp);
+						System.out.println(operators);
+					}
+					
+				}
 				//while loop until you reach open parenthesis
 				/**use while loop to check if current operator
 				 * has the same or higher precendence to one 
@@ -85,37 +101,37 @@ public class Project_2A {
 				 * 
 				 * added !.operators.isEmpty(), as empty stack causes error for comparison
 				 */
-				while(!operators.isEmpty() && (precedence(operators.peek()) > precedence(tokenArray[i]))) {
-					Integer right = nums.pop();
-					Integer left = nums.pop();
-					nums.push(equation(left, right, operators.pop()));
-				}//end while loop
+				
 				
 				//once token is either first token or highest precedence adds to stack
-				operators.push(tokenArray[i]);
-			}//end else if branch for operator
+				
+			}//end else branch for operator
 		}//end for
 		
 		/** once the while expression is either added to stacks or is 
 		 * put through needed operations solves remaining operations in
 		 * the operators stack, until empty
 		 */
-		while (!operators.isEmpty()) {
+		while (!operators.isEmpty() ) {
+			System.out.println(nums);
 			Integer right = nums.pop();
+			System.out.println(nums);
 			Integer left = nums.pop();
-			nums.push(equation(left, right, operators.pop()));
-			
-			return nums.pop();
-		}
+			nums.push(equation(left, right, operators.pop()));				
+		}//end while
+		
+		return nums.pop();
+    
 	}//end evaluate method
 	
-		/*
-		*Precedence method. This returns a value based on the operator 
-		*This value represents the order of operations on the expression so the proper result can be calculated
-		*Takes in the string operater and compares it using operator.equals to find the proper int value of precedence
-		*@Returns: Int representing precendence 
-		*/
-	public int precedence(String operator) {
+	/*
+	*Precedence method. This returns a value based on the operator 
+	*This value represents the order of operations on the expression so the proper result can be calculated
+	*Takes in the string operater and compares it using operator.equals to find the proper int value of precedence
+	*@Returns: Int representing precendence 
+	*/
+	
+	public static int precedence (String operator) {
 		if (operator.equals("^"))                           { return 7; }
 		if (operator.equals("*") || operator.equals("/"))   { return 6; }
 		if (operator.equals("+") || operator.equals("-"))   { return 5; }
@@ -124,36 +140,147 @@ public class Project_2A {
 		if (operator.equals("==") || operator.equals("!=")) { return 3; }
 		if (operator.equals("&&"))                          { return 2; }
 		if (operator.equals("||"))                          { return 1; }		
-		throw new IllegalArgumentException(String.format("Operator %s is not supported.", operator)) }  // Time complexity: O(n)
+		if (operator.contentEquals("("))					{ return 0; }
+		throw new IllegalArgumentException(String.format("Operator %s is not supported.", operator));   // Time complexity: O(n)
         }//End Precedence method
+
+	static public int equation(Integer left, Integer right, String s) {
+		
+		switch(s) {
+        
+		default: return 0;
+	    case "+":
+	        return(left + right);
+	     
+	    
+	    case "-":
+	        return(left - right);
+
+	        
+	    case "*":
+	        return(left * right);
+
+	    case "/":
+	        return(left / right);
+
+	        
+	    case "^":
+	        return (int) (Math.pow(left, right));
+
+	        
+	    case ">":
+	        if(left > right) {
+	            return 1;
+	        }
+	        else {
+	            return 0;
+	        }
+	   
+	        
+	    case "<":
+	        if(left < right) {
+	            return 1;
+	        }
+	        else {
+	            return 0;
+	        }
+	      
+	        
+	    case ">=":
+	        if(left >= right) {
+	            return 1;
+	        }
+	        else {
+	            return 0;
+	        }
+	  
+	        
+	    case "<=":
+	        if(left <= right) {
+	            return 1;
+	        }
+	        else {
+	            return 0;
+	        }
 	
-	public int equation(Integer left, Integer right, String s) {
-		/**
-		 * FIX ME
-		 */
-	}
-	
+	        
+	    case "==":
+	        if(left == right) {
+	            return 1;
+	        }
+	        else {
+	            return 0;
+	        }
+	        
+	        
+	    case "!=":
+	        if(left != right) {
+	            return 1;
+	        }
+	        else {
+	            return 0;
+	        }
+	        
+		}//end switch
+	        
+	   /* case "&&":
+	        if(left && right) {
+	            return 1;
+	        }
+	        
+	        else {
+	            return 0;
+	        }
+	    break;
+	    
+	    case "||":
+	        if((left || right) {
+	            return 1;
+	        }
+	        
+	        else {
+	            return 0;
+	        }
+	    break;
+		}*/
+	}//end equation method
+
 	//method to check if character  is a number
 	//simple method to see if char is between 1-9 and thus a number
-	public boolean isNumber(char c) {
+	static public boolean isNumber(char c) {
 		if(c >= '0' && c <='9')
 			return true;
 		else
 			return false;
 	}//end is number check
+
 	/*
 	*Method to check if String s is equal to any of the allowed operators for this project. 
 	*Returns: True if String s is equal to one of the operators, otherwise returns false bvecause String s isnt an operator
 	*/
-	public boolean isOperator(String s) {
-		if (s.equals("^" || "*" || "/" || "+" || "-" || ">" || ">=" || "<" || "<=" || "==" || "!=" || "&&" || "||"){
-			return true;
-		}else {return false;}
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static public boolean isOperator(String s) {
+		switch (s) {
+		case "^": return true;
+		case "*": return true;
+		case "/": return true;
+		case "+": return true;
+		case "-": return true;
+		case ">": return true;
+		case ">=": return true;
+		case "<=": return true;
+		case "==": return true;
+		case "!=": return true;
+		case "&&": return true;
+		case "||": return true;
+		default: return false;
+		
+		}//end switch
+	}//end isOperator	
+	
+	public static void main(String[] args) throws IOException {
+		//TEST//
+		System.out.println(evaluateEquation("9+5"));
+	}//end main	
 
-	}
-
-}
+}//end class
 
